d6c566666bc40cb4469e1f50025d612ca839383a
pandurang sadgir <psadgir111@gmail.com>
2022-12-24 13:35:52 +0530

    responsiveness of home page

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-console.log(window.innerWidth);
-console.log(window.innerHeight);
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const menus = document.getElementById('menus');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        menus.classList.add('active');
+    })
+}
+
+
+if(close){
+    close.addEventListener('click',()=>{
+        menus.classList.remove('active');
+    })
+}
